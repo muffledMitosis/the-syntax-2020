194064084e30908a9e0913ea019c0bd06b07c2ee
Meth Munindradasa <methsndr@gmail.com>
2020-10-02 07:31:37 +0530

    Complete main landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import mainBack from './assets/web-back-full.png';
+import wordLogo from './assets/word-logo-rccs.png';
+import rectLogo from './assets/rect-logo.png';
+
 function App() {
   return (
-    <div>
-      lol
+    <div className="mainContainer">
+      <img className="rectLogo" src={rectLogo} />
+      <img className="wordLogo" src={wordLogo} />
+      <img className="mainBack" src={mainBack} />
+      
     </div>
   );
 }
