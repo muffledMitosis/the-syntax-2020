a2c58c28df437af77979f710d6e8e0eaee34114a
Meth Munindradasa <methsndr@gmail.com>
2020-10-24 15:52:24 +0530

    Moved consts to a file and added about section

--- src/App.js
+++ src/App.js
@@ -4,27 +4,9 @@ import './App.css';
 
 import analytics from './utils/firebase.js';
 import Countdown from './utils/countdown.js';
+import * as consts from './utils/constStrings.js';
 
 analytics.logEvent('page_visit');
-
-const contentCreation = [
-  "Article content must be at most 2 A4 sheets long",
-  "Articles must include sources/links for both images and content created ( Websites, chat forums, Wikipedia etc)",
-  "Your creations must be relevant to the theme allocated for the particular week you are participating in",
-  "Article Content must be submitted within the 7 days allocated for each theme apon its announcement on the website",
-  "Any acts of plagiarism will result in the participants to be banned from all events conducted under the Royal College Computer Society"
-];
-
-const submission = [
-  "Writers may compete individually (SOLO) or in teams of two (DUO)",
-  "When submitting creations the writer or writers have the choice of including a nickname to be identified by along with their actual identification",
-  "All submissions regardless of school will be accepted upon the appropriate identification details being submitted along with the article",
-  "When submitting kindly submit your articles and source materials in separate documents as you will see when submitting"
-];
-
-const rectLogo = "https://firebasestorage.googleapis.com/v0/b/syntax-2020.appspot.com/o/web-images%2Frect-logo.png?alt=media&token=584c78a9-889a-4859-95ed-1a052023f448";
-const wordLogo = "https://firebasestorage.googleapis.com/v0/b/syntax-2020.appspot.com/o/web-images%2Fword-logo-rccs.png?alt=media&token=e3e75061-f5f8-4d39-9552-1f29a2fce909";
-const mainBack = "https://firebasestorage.googleapis.com/v0/b/syntax-2020.appspot.com/o/web-images%2FfullBackLogoAddon.jpg?alt=media&token=e9af64fd-1151-4e08-ab4f-88faeabecc3b";
 
 function App() {
 
@@ -33,21 +15,23 @@ function App() {
       <Parallax strength={200}>
            <div className="testContainer">
             <div className="mainContainer">
-              <img className="rectLogo" src={rectLogo} />
-              <img className="wordLogo" src={wordLogo} />
+              <img className="rectLogo" src={consts.rectLogo} />
+              <img className="wordLogo" src={consts.wordLogo} />
 
             </div>
            </div>
            <Background className="custom-bg">
-            <img className="mainBack" src={mainBack} />
+            <img className="mainBack" src={consts.mainBack} />
            </Background>
       </Parallax>
 
       <div className="rulesAndRegulations baseGradient">
+        <h2>What Is Syntax?</h2>
+        {consts.intro.map(item=><p>{item}</p>)}
         <h2>Content Creation Guidelines</h2>
-        <ul>{contentCreation.map(item=><li>{item}</li>)}</ul>
+        <ul>{consts.contentCreation.map(item=><li>{item}</li>)}</ul>
         <h2>Submission Guidelines</h2>
-        <ul>{submission.map(item=><li>{item}</li>)}</ul>
+        <ul>{consts.submission.map(item=><li>{item}</li>)}</ul>
       </div>
       
       <div className="baseGradient"><Countdown /><h3>more...</h3></div>
---
+++ src/utils/constStrings.js
@@ -0,0 +1,29 @@
+const rectLogo = "https://firebasestorage.googleapis.com/v0/b/syntax-2020.appspot.com/o/web-images%2Frect-logo.png?alt=media&token=584c78a9-889a-4859-95ed-1a052023f448";
+const wordLogo = "https://firebasestorage.googleapis.com/v0/b/syntax-2020.appspot.com/o/web-images%2Fword-logo-rccs.png?alt=media&token=e3e75061-f5f8-4d39-9552-1f29a2fce909";
+const mainBack = "https://firebasestorage.googleapis.com/v0/b/syntax-2020.appspot.com/o/web-images%2FfullBackLogoAddon.jpg?alt=media&token=e9af64fd-1151-4e08-ab4f-88faeabecc3b";
+
+const contentCreation = [
+    "Article content must be at most 2 A4 sheets long",
+    "Articles must include sources/links for both images and content created ( Websites, chat forums, Wikipedia etc)",
+    "Your creations must be relevant to the theme allocated for the particular week you are participating in",
+    "Article Content must be submitted within the 7 days allocated for each theme apon its announcement on the website",
+    "Any acts of plagiarism will result in the participants to be banned from all events conducted under the Royal College Computer Society"
+];
+
+const submission = [
+    "Writers may compete individually (SOLO) or in teams of two (DUO)",
+    "When submitting creations the writer or writers have the choice of including a nickname to be identified by along with their actual identification",
+    "All submissions regardless of school will be accepted upon the appropriate identification details being submitted along with the article",
+    "When submitting kindly submit your articles and source materials in separate documents as you will see when submitting",
+    "The document format must be word document or google docs. Non editable documents will not be accepted. ( PDF)"
+];
+
+const intro = [
+    "The Syntax Magazine is a technological publication in which multitudes of creative thinkers come together to celebrate the technological evolution made by humanity in the many varieties of industries and disciplines related to the vast subject.",
+    "This year \'The Syntax\' magazine will be hosted online with many new features for our readers. Readers will be given the opportunity to compete for limited space allocated space dedicated to unique creations/articles based on 4 seperate themes. Each theme will be allocated over the next 4 weeks with a 7 day time limit.",
+    "Interested parties have the independence to participate as an individual by going 'SOLO' or as a 2 man team by forming a 'DUO'.",
+    "Everyone is encouraged to participate to the best of their ability regardless of age or school. Writers can write on anything ranging from tutorial bases creation and upcoming tech news to personal ideas that you or your team believe will change the world.",
+    "Those who succeed with their creations will get the chance see their creations on the 'The Syntax' E-Magazine of 2020. Those who miss the ticket will be rewarded for their perseverance throughout the competition."
+];
+
+export { rectLogo, wordLogo, mainBack, contentCreation, submission, intro }
